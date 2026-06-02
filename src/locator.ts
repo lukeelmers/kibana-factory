760079dcf82c1e3852f1f5ef460018b66cffbb70
elastibot <luke.elmers+github+agent@elasticsearch.com>
2026-06-02 10:38:49 -0600

    Fix: Encode special characters in locator navigation URLs

---
+++ src/locator.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export interface LocatorParams {
+  [key: string]: string;
+}
+
+export interface Locator {
+  navigate(params: LocatorParams): void;
+  getUrl(params: LocatorParams): string;
+}
+
+export function createLocator(basePath: string): Locator {
+  function getUrl(params: LocatorParams): string {
+    const url = new URL(basePath, window.location.origin);
+    const searchParams = new URLSearchParams();
+
+    for (const [key, value] of Object.entries(params)) {
+      searchParams.set(key, value);
+    }
+
+    url.search = searchParams.toString();
+    return url.pathname + url.search + url.hash;
+  }
+
+  function navigate(params: LocatorParams): void {
+    const url = getUrl(params);
+    window.location.assign(url);
+  }
+
+  return { navigate, getUrl };
+}
